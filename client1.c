01e876e1bc617579fc5880707672ad57172d453b
tt8cactus <1016872396@qq.com>
2022-05-14 10:46:18 +0800

    1

--- client1.c
+++ client1.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 
 #define MAXLINE 4096 /*max text line length*/
-#define SERV_PORT 3000 /*port*/
+#define SERV_PORT 30000 /*port*/
 
 int main(int argc, char **argv)
 {
@@ -15,27 +15,21 @@ int main(int argc, char **argv)
     struct sockaddr_in servaddr;
     char sendline[MAXLINE], recvline[MAXLINE];
     
-    //basic check of the arguments
-    //additional checks can be inserted
     if (argc !=2) {
         perror("Usage: TCPClient <IP address of the server");
         exit(1);
     }
     
-    //Create a socket for the client
-    //If sockfd<0 there was an error in the creation of the socket
     if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
         perror("Problem in creating the socket");
         exit(2);
     }
     
-    //Creation of the socket
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr= inet_addr(argv[1]);
     servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
     
-    //Connection of the client to the socket
     if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
         perror("Problem in connecting to the server");
         exit(3);
@@ -43,7 +37,7 @@ int main(int argc, char **argv)
     
     while (fgets(sendline, MAXLINE, stdin) != NULL) {
         send(sockfd, sendline, strlen(sendline), 0);
-        recv(sockfd, recvline, MAXLINE,0) == 0);
+        recv(sockfd, recvline, MAXLINE,0);
         printf("%s", "String received from the server: ");
         fputs(recvline, stdout);
     }
